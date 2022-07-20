cf42571a225f315c274123a11e854bc8e25bff0f
Alexey <lexaxx14@gmail.com>
2022-07-20 16:18:03 +0000

    script: Upload kernel to Telegram
    Signed-off-by: Alexey <lexaxx14@gmail.com>

--- compile.sh
+++ compile.sh
@@ -4,12 +4,15 @@
 # Copyright (C) 2020-2021 Adithya R.
 
 SECONDS=0 # builtin bash timer
-ZIPNAME="SunriseKernel-juice-4.19.160.zip"
+ZIPNAME="/tmp/output/SunriseKernel-juice_$(date +%Y%m%d-%H%M).zip"
 TC_DIR="$HOME/tc/sdclang"
 GCC_DIR="$HOME/tc/gcc"
 GCC64_DIR="$HOME/tc/gcc64"
 AK3_DIR="$HOME/android/AnyKernel3"
 DEFCONFIG="vendor/bengal-perf_defconfig"
+
+export TELEGRAM_BOT_TOKEN=""
+export TELEGRAM_CHAT_ID=""
 
 export PATH="${TC_DIR}/compiler/bin:${GCC64_DIR}/bin:${GCC_DIR}/bin:/usr/bin:${PATH}"
 
@@ -68,14 +71,14 @@ cp out/arch/arm64/boot/Image AnyKernel3
 rm -f *zip
 cd AnyKernel3
 git checkout master &> /dev/null
-zip -r9 "../$ZIPNAME" * -x '*.git*' README.md *placeholder
+zip -r9 "$ZIPNAME" * -x '*.git*' README.md *placeholder
 cd ..
 rm -rf AnyKernel3
 rm -rf out/arch/arm64/boot
 echo -e "\nCompleted in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) !"
 echo "Zip: $ZIPNAME"
 if ! [[ $HOSTNAME = "enprytna" && $USER = "endi" ]]; then
-curl --upload-file $ZIPNAME https://temp.sh/$ZIPNAME; echo
+curl -F document=@"${ZIPNAME}" -F "caption=${FILE_CAPTION}" "https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendDocument?chat_id=${TELEGRAM_CHAT_ID}&parse_mode=Markdown"
 fi
 else
 echo -e "\nCompilation failed!"
